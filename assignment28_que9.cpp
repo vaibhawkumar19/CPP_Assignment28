c335220421112dfa680c13bb8190db568e8eb584
Vaibhaw Kumar <95565240+vaibhawkumar19@users.noreply.github.com>
2022-10-03 13:57:30 +0530

    Add files via upload

---
+++ assignment28_que9.cpp
@@ -0,0 +1,25 @@
+/*9. Create an Integer class that contains int x as an instance variable and overload
+casting int() operator that will type cast your Integer class object to int data type.*/
+#include<iostream>
+using namespace std;
+class Integer
+{
+	int x;
+	public:
+		void set(int x)
+		{
+			this->x=x;
+		}
+		int operator()()
+		{	
+			return x;
+		}
+};
+int main()
+{
+	Integer i;
+	i.set(4);
+	i.operator();
+	cout<<i()<<endl;
+return 0;
+}
